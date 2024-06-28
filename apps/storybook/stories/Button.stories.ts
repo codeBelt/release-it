611aaa477656073b50118171c5f2a51d6382fc0e
Duarte Monteiro <du5rtemonteiro@gmail.com>
2024-06-28 21:46:09 +0100

    chore(storybook): follow storybook tutorial

--- apps/storybook/stories/Button.stories.ts
+++ apps/storybook/stories/Button.stories.ts
@@ -1,8 +1,9 @@
-import { fn } from '@storybook/test';
-import { Button } from './Button';
+import type { Meta, StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
+import { Button } from './Button'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-export default {
+const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   parameters: {
@@ -17,32 +18,44 @@ export default {
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
-};
+}
+
+export default meta
+
+type Story = StoryObj<typeof Button>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary = {
+export const Primary: Story = {
   args: {
     primary: true,
     label: 'Button',
   },
-};
+}
 
-export const Secondary = {
+export const Secondary: Story = {
   args: {
     label: 'Button',
   },
-};
+}
 
-export const Large = {
+export const Warning: Story = {
+  args: {
+    primary: true,
+    label: 'Delete now',
+    backgroundColor: 'red',
+  },
+}
+
+export const Large: Story = {
   args: {
     size: 'large',
     label: 'Button',
   },
-};
+}
 
 export const Small = {
   args: {
     size: 'small',
     label: 'Button',
   },
-};
+}
