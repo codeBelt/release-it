856bbc1f41b8fe5672e2e797bc1d58757e42c757
Duarte Monteiro <du5rtemonteiro@gmail.com>
2024-06-28 23:36:41 +0100

    chore: format files

--- apps/storybook/stories/Header.stories.js
+++ apps/storybook/stories/Header.stories.js
@@ -1,5 +1,5 @@
-import { fn } from '@storybook/test';
-import { Header } from './Header';
+import { fn } from '@storybook/test'
+import { Header } from './Header'
 
 export default {
   title: 'Example/Header',
@@ -15,15 +15,15 @@ export default {
     onLogout: fn(),
     onCreateAccount: fn(),
   },
-};
+}
 export const LoggedIn = {
   args: {
     user: {
       name: 'Jane Doe',
     },
   },
-};
+}
 
 export const LoggedOut = {
   args: {},
-};
+}
