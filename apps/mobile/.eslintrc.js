3c7ea2f0f4695266fc407b2e7cb792404e1fabe8
Duarte Monteiro <du5rtemonteiro@gmail.com>
2024-06-28 12:02:19 +0100

    refactor(eslint-config): refactor expo config

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -1,17 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'warn',
-  },
+  root: true,
+  extends: ['@repo/eslint-config/expo.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    '.*.js',
-    'node_modules/',
-  ],
 }
---
+++ packages/eslint-config/expo.js
@@ -0,0 +1,24 @@
+const { resolve } = require('node:path')
+
+const project = resolve(process.cwd(), 'tsconfig.json')
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ['eslint:recommended', 'expo', 'prettier'],
+  plugins: ['prettier', 'only-warn'],
+  rules: {
+    'prettier/prettier': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    '.*.js',
+    'node_modules/',
+  ],
+}
